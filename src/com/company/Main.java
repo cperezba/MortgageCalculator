a06aefe9170b38180385072acf3a7b5066a810d3
Carlos Perez <ca.perez.barr@gmail.com>
2021-12-30 10:09:47 -0600

    Update: Data was transferred from alternate file.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,77 @@
 package com.company;
 
+import java.text.NumberFormat;
+import java.util.Scanner;
+
 public class Main {
+    final static byte MONTHS_IN_YEAR = 12;
+    final static byte PERCENT = 100;
+
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scanner = new Scanner(System.in);
+
+
+        double principal = (double) userPrompt("Principal ($1k - $1M): ", 1_000, 1_000_000);
+        double annualRate = (double) userPrompt("Annual Interest Rate (1 - 30): ", 1, 30);
+        double periodInYrs = (double) userPrompt("Period(Years) (1 - 30): ", 1, 30);
+
+
+        printMortgage(principal, annualRate, periodInYrs);
+        printPaymentSchedule(principal, annualRate, periodInYrs);
+    }
+
+
+    private static void printMortgage(double principal, double annualRate, double periodInYrs) {
+        String mortgageFormatted = NumberFormat.getCurrencyInstance().format(calculateMortgage(principal, annualRate, periodInYrs));
+        System.out.println("MORTGAGE");
+        System.out.println("--------");
+        System.out.println("Monthly Payments: " + mortgageFormatted);
+    }
+
+
+    private static void printPaymentSchedule(double principal, double annualRate, double periodInYrs) {
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("----------------");
+        for (short month = 1; month <= periodInYrs * MONTHS_IN_YEAR; month++) {
+            double balance = calculateBalance(principal, annualRate, periodInYrs, month);
+
+            System.out.println(NumberFormat.getCurrencyInstance().format(balance));
+        };
+    }
+
+
+    public static double userPrompt(String message, int min, int max) {
+        Scanner scanner = new Scanner(System.in);
+        double value;
+        while (true) {
+            System.out.print(message);
+            value = scanner.nextDouble();
+            if (value >= min && value <= max)
+                break;
+            System.out.print("Enter a number between " + min + " and " + max + ".\n");
+        }
+        return value;
+    }
+
+    public static double calculateMortgage(double principal, double annualRate, double periodInYrs) {
+        final double monthlyInterest = annualRate / PERCENT / MONTHS_IN_YEAR;
+        final double numberOfPayments = periodInYrs * MONTHS_IN_YEAR;
+
+        double mortgage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)) / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        return mortgage;
+    }
+
+    public static double calculateBalance(double principal, double annualRate, double periodInYrs, double numberOfPaymentsMade) {
+        final double monthlyInterest = annualRate / PERCENT / MONTHS_IN_YEAR;
+        final double numberOfPayments = periodInYrs * MONTHS_IN_YEAR;
+
+        double balance = principal
+                * (Math.pow(1 + monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        return balance;
 
     }
 }
